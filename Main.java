a652e0dd162943a1be2fac86f98b545acf75f75f
Bryce? <bryce.jones2026@gbwl.org>
2025-05-12 13:40:35 +0000

    START

--- Main.java
+++ Main.java
@@ -1,6 +1,58 @@
+import java.util.Scanner;
+
 public class Main {
 
+   
+
+
    public static void main(String []args) {
-      System.out.println("Hello World");
+    Scanner scan = new Scanner(System.in);
+    
+    String className;
+
+
+
+    System.out.println("Pick a race: orc, elf, human, tree, tiefling, gnome, dwarf, halfling, and gundam.");
+    String command = scan.nextLine();
+
+    if (command.equals("orc")){
+      String race = command;
+      System.out.print(race);
+    }
+    else if (command.equals("elf")){
+      String race = command;
+      System.out.print(race);
+    }
+    else if (command.equals("human")){
+      String race = command;
+      System.out.print(race);
+    }
+    else if (command.equals("tree")){
+      String race = command;
+      System.out.print(race);
+    }
+    else if (command.equals("tiefling")){
+      String race = command;
+      System.out.print(race);
+    }
+    else if (command.equals("gnome")){
+      String race = command;
+      System.out.print(race);
+    }
+    else if (command.equals("dwarf")){
+      String race = command;
+      System.out.print(race);
+    }
+    else if (command.equals("halfling")){
+      String race = command;
+      System.out.print(race);
+    }
+    else if (command.equals("gundam")){
+      String race = command;
+      System.out.print(race);
+    }
+
+
+
    }
 }
