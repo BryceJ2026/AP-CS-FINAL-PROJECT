5b22e565499cc98b57d7c0b55d86acc702d6012c
Bryce? <bryce.jones2026@gbwl.org>
2025-05-13 12:55:13 +0000

    race done

--- Main.java
+++ Main.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class Main {
 
    
-
-
    public static void main(String []args) {
     Scanner scan = new Scanner(System.in);
     
@@ -17,40 +15,51 @@ public class Main {
 
     if (command.equals("orc")){
       String race = command;
-      System.out.print(race);
+      System.out.print("You picked " + race);
     }
     else if (command.equals("elf")){
       String race = command;
-      System.out.print(race);
+      System.out.print("You picked " + race);
     }
     else if (command.equals("human")){
       String race = command;
-      System.out.print(race);
+      System.out.print("You picked " + race);
     }
     else if (command.equals("tree")){
       String race = command;
-      System.out.print(race);
+      System.out.print("You picked " + race);
     }
     else if (command.equals("tiefling")){
       String race = command;
-      System.out.print(race);
+      System.out.print("You picked " + race);
     }
     else if (command.equals("gnome")){
       String race = command;
-      System.out.print(race);
+      System.out.println("You picked " + race);
     }
     else if (command.equals("dwarf")){
       String race = command;
-      System.out.print(race);
+      System.out.println("You picked " + race);
     }
     else if (command.equals("halfling")){
       String race = command;
-      System.out.print(race);
+      System.out.println("You picked " + race);
     }
     else if (command.equals("gundam")){
       String race = command;
-      System.out.print(race);
+      System.out.println("You picked " + race);
     }
+    else if (command.equals("Tyler the Creator")){
+      System.out.println("PEAK");
+    }
+    else if (command.equals("ye")){
+      System.out.println("");
+    }
+    else {
+      System.out.println("Learn how to spell, dingus. Should should reload, NOW!");
+    }
+
+    
 
 
 
