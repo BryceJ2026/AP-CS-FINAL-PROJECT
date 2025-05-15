cf9fcd59da467d075f6365b4862127aee102d8e9
Bryce? <bryce.jones2026@gbwl.org>
2025-05-15 12:55:13 +0000

    CLASSES DONE

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
    public static void main(String []args) {
     Scanner scan = new Scanner(System.in);
     
-    String className;
+    
 
 
 
@@ -15,64 +15,401 @@ public class Main {
 
     if (command.equals("orc")){
       String race = command;
-      System.out.print("You picked " + race);
-      String command = scan.nextLine();
+      System.out.println("You picked " + race);
+      
 
-      System.out.println("Now, pick a class. Wizard, Fighter, Monk, Virgil from DMC5, Gun, Shadow the hedgehog, druid, fighter, barbarian, rogue,");
+      System.out.println("Now, pick a class. Wizard, Fighter, Monk, Virgil from DMC5, Gun, Shadow the hedgehog, Druid, Fighter, Barbarian, Rogue, Warlock");
+       command = scan.nextLine();
+      if(command.equals("Wizard")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Fighter")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Monk")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Virgil from DMC5")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Gun")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Shadow the hedgehog")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Druid")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Warlock")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Druid")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Barbarian")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      
 
     }
     else if (command.equals("elf")){
       String race = command;
       System.out.print("You picked " + race);
+      System.out.println("Now, pick a class. Wizard, Fighter, Monk, Virgil from DMC5, Gun, Shadow the hedgehog, Druid, Fighter, Barbarian, Rogue, Warlock");
+       command = scan.nextLine();
+      if(command.equals("Wizard")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Fighter")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Monk")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Virgil from DMC5")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Gun")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Shadow the hedgehog")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Druid")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Warlock")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Druid")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Barbarian")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
     }
     else if (command.equals("human")){
       String race = command;
       System.out.print("You picked " + race);
+      System.out.println("Now, pick a class. Wizard, Fighter, Monk, Virgil from DMC5, Gun, Shadow the hedgehog, Druid, Fighter, Barbarian, Rogue, Warlock");
+       command = scan.nextLine();
+      if(command.equals("Wizard")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Fighter")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Monk")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Virgil from DMC5")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Gun")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Shadow the hedgehog")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Druid")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Warlock")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Druid")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Barbarian")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
     }
     else if (command.equals("tree")){
       String race = command;
       System.out.print("You picked " + race);
+      System.out.println("Now, pick a class. Wizard, Fighter, Monk, Virgil from DMC5, Gun, Shadow the hedgehog, Druid, Fighter, Barbarian, Rogue, Warlock");
+       command = scan.nextLine();
+      if(command.equals("Wizard")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Fighter")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Monk")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Virgil from DMC5")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Gun")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Shadow the hedgehog")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Druid")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Warlock")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Druid")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Barbarian")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
     }
     else if (command.equals("tiefling")){
       String race = command;
       System.out.print("You picked " + race);
+      System.out.println("Now, pick a class. Wizard, Fighter, Monk, Virgil from DMC5, Gun, Shadow the hedgehog, Druid, Fighter, Barbarian, Rogue, Warlock");
+       command = scan.nextLine();
+      if(command.equals("Wizard")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Fighter")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Monk")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Virgil from DMC5")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Gun")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Shadow the hedgehog")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Druid")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Warlock")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Druid")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Barbarian")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
     }
     else if (command.equals("gnome")){
       String race = command;
       System.out.println("You picked " + race);
+      System.out.println("Now, pick a class. Wizard, Fighter, Monk, Virgil from DMC5, Gun, Shadow the hedgehog, Druid, Fighter, Barbarian, Rogue, Warlock");
+       command = scan.nextLine();
+      if(command.equals("Wizard")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Fighter")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Monk")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Virgil from DMC5")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Gun")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Shadow the hedgehog")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Druid")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Warlock")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Druid")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Barbarian")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
     }
     else if (command.equals("dwarf")){
       String race = command;
       System.out.println("You picked " + race);
+      System.out.println("Now, pick a class. Wizard, Fighter, Monk, Virgil from DMC5, Gun, Shadow the hedgehog, Druid, Fighter, Barbarian, Rogue, Warlock");
+       command = scan.nextLine();
+      if(command.equals("Wizard")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Fighter")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Monk")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Virgil from DMC5")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Gun")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Shadow the hedgehog")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Druid")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Warlock")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Druid")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Barbarian")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
     }
     else if (command.equals("halfling")){
       String race = command;
       System.out.println("You picked " + race);
+      System.out.println("Now, pick a class. Wizard, Fighter, Monk, Virgil from DMC5, Gun, Shadow the hedgehog, Druid, Fighter, Barbarian, Rogue, Warlock");
+       command = scan.nextLine();
+      if(command.equals("Wizard")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Fighter")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Monk")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Virgil from DMC5")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Gun")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Shadow the hedgehog")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Druid")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Warlock")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Druid")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
+      if(command.equals("Barbarian")){
+        String className = command;
+        System.out.println("You picked " + className + "!");
+      }
     }
     else if (command.equals("gundam")){
       String race = command;
-      System.out.println("You picked " + race);
+      System.out.println("You fly off into space. THE GUNDAM ENDING");
+      
     }
     else if (command.equals("Tyler the Creator")){
-      System.out.println("PEAK");
+      System.out.println("PEAK. THE TYLER ENDING");
     }
     else if (command.equals("ye")){
-      System.out.println("We miss the old kanye");
+      System.out.println("We miss the old kanye. THE YE ENDING");
     }
     else if (command.equals("Mario")){
-      System.out.println("Go back to the mushroom kingdom");
+      System.out.println("Go back to the mushroom kingdom. THE MARIO ENDING");
     }
     else if (command.equals("Luigi")){
-      System.out.println("L is real");
+      System.out.println("L is real. THE L ENDING");
     }
     else if (command.equals("Toad")){
-      System.out.println("Get out. Leave. We don't want you here.");
+      System.out.println("Get out. Leave. We don't want you here. THE TOAD ENDING");
     }
     else if (command.equals("Secret")){
-      System.out.println("Oh wow, look at the special little baby trying to be cool. Get out of my sight.");
+      System.out.println("Oh wow, look at the special little baby trying to be cool. Get out of my sight. THE 'SECRET' ENDING");
     }
     else {
-      System.out.println("Learn how to spell, dingus. Should should reload, NOW!");
+      System.out.println("Learn how to spell, dingus. YOU should reload, NOW! THE DINGUS ENDING");
     }
 
     
